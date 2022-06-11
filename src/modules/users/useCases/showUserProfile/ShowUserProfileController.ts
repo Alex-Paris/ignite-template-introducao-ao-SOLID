5f25f53b0a51cb7b30aa1c0bf2c1657ddb7b3ef1
Alex Paris <alex_paris28@hotmail.com>
2022-06-11 10:36:29 -0300

    chore: task completed

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -2,11 +2,21 @@ import { Request, Response } from "express";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
+/* eslint @typescript-eslint/no-explicit-any: ["off"] */
+
 class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id }: any = request.params;
+
+    try {
+      const all = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -2,11 +2,21 @@ import { Request, Response } from "express";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
+/* eslint @typescript-eslint/no-explicit-any: ["off"] */
+
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id }: any = request.params;
+
+    try {
+      const all = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
