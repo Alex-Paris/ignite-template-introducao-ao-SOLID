61757bd583aa015e7400e36fa418532bdb724556
Alex Paris <alex_paris28@hotmail.com>
2022-06-11 12:07:04 -0300

    fix: var name

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -11,9 +11,9 @@ class ShowUserProfileController {
     const { user_id }: any = request.params;
 
     try {
-      const all = this.showUserProfileUseCase.execute({ user_id });
+      const user = this.showUserProfileUseCase.execute({ user_id });
 
-      return response.json(all);
+      return response.json(user);
     } catch (err) {
       return response.status(404).json({ error: err.message });
     }
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -11,9 +11,9 @@ class TurnUserAdminController {
     const { user_id }: any = request.params;
 
     try {
-      const all = this.turnUserAdminUseCase.execute({ user_id });
+      const user = this.turnUserAdminUseCase.execute({ user_id });
 
-      return response.json(all);
+      return response.json(user);
     } catch (err) {
       return response.status(404).json({ error: err.message });
     }
